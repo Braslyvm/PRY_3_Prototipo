a3b6af30eb9ae958bdaf07e62a9ba9a63b453b07
Eyden Su <eydendiaz81@gmail.com>
2024-10-27 21:41:37 -0600

    cambios en el diseño

--- PRY_3/obj/Debug/net8.0/PRY_3.AssemblyInfo.cs
+++ PRY_3/obj/Debug/net8.0/PRY_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRY_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08163f49e6037a2b5101615354636855c3145ef0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+dd555a98d12ec38053a552ca15e1b2f757fc2908")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRY_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRY_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
