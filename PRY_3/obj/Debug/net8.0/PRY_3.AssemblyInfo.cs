33ec7ef6d99d725a73be3f865587796ba4a1323e
ELDERPEREZ <colegioempresarial20@gmail.com>
2024-11-04 14:05:21 -0600

    .

--- PRY_3/obj/Debug/net8.0/PRY_3.AssemblyInfo.cs
+++ PRY_3/obj/Debug/net8.0/PRY_3.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("PRY_3")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+6dd0fde582861ecf571023588d938d202a921c08")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e129d2f76c5cbf4cfca2f3f5958fbc7aea401a54")]
 [assembly: System.Reflection.AssemblyProductAttribute("PRY_3")]
 [assembly: System.Reflection.AssemblyTitleAttribute("PRY_3")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
