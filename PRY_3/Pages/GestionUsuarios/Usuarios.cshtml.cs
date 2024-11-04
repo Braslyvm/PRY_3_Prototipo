c9cedfb750f189f63539d706ea85e9b68c323642
Braslyvm <braslymorales@gmail.com>
2024-11-03 21:20:30 -0600

    actualizacion

--- PRY_3/Pages/GestionUsuarios/Usuarios.cshtml.cs
+++ PRY_3/Pages/GestionUsuarios/Usuarios.cshtml.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Threading.Tasks;
 
 namespace PRY_3.Pages
@@ -13,15 +14,37 @@ namespace PRY_3.Pages
 
         [BindProperty]
         public User NewUser { get; set; } = new User();
+        public string Role { get; set; }
+        public string Estado { get; set; }
+        public string Sexo { get; set; }
 
-        public async Task<IActionResult> OnGetAsync()
-        {
+        public async Task<IActionResult> OnGetAsync(){
             getusuarios();
             return Page();
         }
 
         public IActionResult OnPostBorrar(int cedula){
             borrar(cedula);
+            return RedirectToPage();
+        }
+
+        public IActionResult OnPostModificar(int cedula, string userName, string apellido_1, string apellido_2, string sexo, string lugar_Recidencia, string estado, string rol, string correo){ 
+            string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "usuarios.txt");
+            var lines = System.IO.File.ReadAllLines(filePath);
+            var updatedLines = new List<string>();
+            foreach (var line in lines)
+            {
+                var values = line.Split(',');
+                if (values.Length == 9 && int.Parse(values[0]) == cedula){
+                    updatedLines.Add($"{cedula},{userName},{apellido_1},{apellido_2},{sexo},{lugar_Recidencia},{rol},{estado},{correo}");
+                }
+                else{
+                    updatedLines.Add(line);
+                }
+            }
+            System.IO.File.WriteAllLines(filePath, updatedLines);
+
+            // Redirigir a la página actual después de la actualización
             return RedirectToPage();
         }
         public void borrar(int cedula){
@@ -35,7 +58,6 @@ namespace PRY_3.Pages
             System.IO.File.WriteAllLines(filePath, updatedLines);
         }
 
-
         public void getusuarios()
         {
             string filePath = Path.Combine(Directory.GetCurrentDirectory(), "wwwroot", "usuarios.txt");
@@ -44,14 +66,19 @@ namespace PRY_3.Pages
             foreach (var line in lines)
             {
                 var values = line.Split(',');
-                if (values.Length == 4)
+                if (values.Length == 9)
                 {
-                    users.Add(new User
-                    {
+                    users.Add(new User{
                         Cedula = int.Parse(values[0]),
                         UserName = values[1],
-                        Email = values[2],
-                        Role = values[3]
+                        Apellido_1 = values[2],
+                        Apellido_2 = values[3],
+                        Sexo = values[4],
+                        Lugar_Recidencia = values[5],
+                        Role = values[6],
+                        Estado = values[7],
+                        correo = values[8]
+                        
                     });
                 }
             }
@@ -62,7 +89,12 @@ namespace PRY_3.Pages
     {
         public int Cedula { get; set; }
         public string UserName { get; set; }
-        public string Email { get; set; }
+        public string Apellido_1 { get; set; }
+        public string Apellido_2 { get; set; }
+        public string Sexo { get; set; }
+        public string Lugar_Recidencia { get; set; }
         public string Role { get; set; }
+        public string Estado { get; set; }
+        public string correo { get; set; }
     }
 }
